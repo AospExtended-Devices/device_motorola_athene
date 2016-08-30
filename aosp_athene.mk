0e8de6ede577b0dc8b6819b3eb72f0e2a186e08d
Shreps <shr3ps@gmail.com>
2016-08-30 14:31:38 +0200

    athene: Add TWRP Support
      To build TWRP, successively run the following commands from the top of your
      LineageOS root / source directory:
    
         * git clone https://github.com/LineageOS/android_external_busybox external/busybox
         * git clone https://github.com/OmniROM/android_bootable_recovery bootable/recovery-twrp
         * source build/envsetup.sh
         * breakfast athene
         * export WITH_TWRP=true
         * mka recoveryimage
    
    Change-Id: I65eaedbe3b1d990f8d46ca9f6c697d254d321290

--- aosp_athene.mk
+++ aosp_athene.mk
@@ -18,6 +18,8 @@ $(call inherit-product, vendor/aosp/common.mk)
 
 $(call inherit-product, device/motorola/athene/full_athene.mk)
 
+$(call inherit-product, device/motorola/athene/twrp.mk)
+
 # Boot animation
 TARGET_SCREEN_WIDTH := 1080
 TARGET_SCREEN_HEIGHT := 1920
